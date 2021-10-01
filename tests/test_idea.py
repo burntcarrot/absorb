2f5249e75f6bbae69b7eedb4f6983242c80fe5cf
Andre Decher uidr3545 <andre.decher@bsk-germany.com>
2021-10-01 08:08:40 +0200

    Increase overall coverage to 92 percent

--- tests/test_idea.py
+++ tests/test_idea.py
@@ -1,13 +1,19 @@
 import click.testing
 import pytest
+import os
+from pathlib import Path
 from click.testing import CliRunner
 from absorb.core.idea.commands import idea
+from absorb.config.paths import ROOT_PATH
 
 
 @pytest.fixture
 def runner() -> CliRunner:
     return click.testing.CliRunner()
 
+file_path = os.fspath(
+    Path(Path.cwd() / Path(Path("tests") / Path("files") / "idea_description.txt"))
+)
 
 # Tests for new() (new idea)
 def test_idea_new(runner: CliRunner) -> None:
@@ -17,6 +23,19 @@ def test_idea_new(runner: CliRunner) -> None:
     )
     assert result.exit_code == 0
 
+def test_idea_new_empty_description(runner: CliRunner) -> None:
+    """Adding an idea normally should return 0, if it is valid in nature."""
+    result = runner.invoke(
+        idea, ["new", ".", ".", "."]
+    )
+    assert result.exit_code == 0
+
+def test_idea_new_description_file(runner: CliRunner) -> None:
+    """Adding an idea with decription file should return 0, if it is valid in nature."""
+    result = runner.invoke(
+        idea, ["new", "Make a cool machine!", "+file", "@file"], input=file_path
+    )
+    assert result.exit_code == 0
 
 def test_idea_new_no_tags(runner: CliRunner) -> None:
     """Adding an idea with no tags should return 0, if it is valid in nature."""
@@ -25,6 +44,15 @@ def test_idea_new_no_tags(runner: CliRunner) -> None:
     )
     assert result.exit_code == 0
 
+def test_idea_new_FileNotFoundError(runner: CliRunner) -> None:
+    """Adding an idea normally but ideas.json doesnt exist should return 0, if it is valid in nature."""
+    os.rename(ROOT_PATH / "ideas.json", ROOT_PATH / "_ideas.json")
+    result = runner.invoke(
+        idea, ["new", "Make a cool machine!", "Some description.", "@ideas"]
+    )
+    assert result.exit_code == 0
+    os.remove(ROOT_PATH / "ideas.json")
+    os.rename(ROOT_PATH / "_ideas.json", ROOT_PATH / "ideas.json")
 
 # Tests for edit() (edit idea)
 def test_idea_edit(runner: CliRunner) -> None:
@@ -55,12 +83,27 @@ def test_idea_edit_only_name(runner: CliRunner) -> None:
     result = runner.invoke(idea, ["edit", "#1", "Make a super cool machine!", ".", "."])
     assert result.exit_code == 0
 
+def test_idea_edit_with_description_file(runner: CliRunner) -> None:
+    """Editing an idea with description file should return 0, if it is valid in nature."""
+    result = runner.invoke(
+        idea, ["edit", "#1", "Make a super cool machine!", "+file", "."], input=file_path
+    )
+    assert result.exit_code == 0
 
 def test_idea_edit_only_tags(runner: CliRunner) -> None:
     """Editing an idea's tags with no description or name should return 0, if it is valid in nature."""
     result = runner.invoke(idea, ["edit", "#1", ".", ".", "@create"])
     assert result.exit_code == 0
 
+def test_idea_edit_with_no_tags_FileNotFoundError(runner: CliRunner) -> None:
+    """Editing an idea with no tags but ideas.json doesnt exist should return 0, if it is valid in nature."""
+    os.rename(ROOT_PATH / "ideas.json", ROOT_PATH / "_ideas.json")
+    result = runner.invoke(
+        idea, ["edit", "#1", "Make a super cool machine!", "New description.", "."]
+    )
+    assert result.exit_code == 0
+    os.remove(ROOT_PATH / "ideas.json")
+    os.rename(ROOT_PATH / "_ideas.json", ROOT_PATH / "ideas.json")
 
 # Tests for open() (open idea)
 def test_idea_open(runner: CliRunner) -> None:
@@ -68,9 +111,30 @@ def test_idea_open(runner: CliRunner) -> None:
     result = runner.invoke(idea, ["open", "#1"])
     assert result.exit_code == 0
 
+# this will cover absorb\utils\idea_utils.py 58 but only works if the
+# #2 idea has . as description
+def test_idea_open_2(runner: CliRunner) -> None:
+    """Opening an idea should return 0, if it is valid in nature."""
+    result = runner.invoke(idea, ["open", "#2"])
+    assert result.exit_code == 0
+
+def test_idea_open_FileNotFoundError(runner: CliRunner) -> None:
+    """Opening an idea bit ideas.json doesnt exist should return 0, if it is valid in nature."""
+    os.rename(ROOT_PATH / "ideas.json", ROOT_PATH / "_ideas.json")
+    result = runner.invoke(idea, ["open", "#2"])
+    assert result.exit_code == 0
+    os.rename(ROOT_PATH / "_ideas.json", ROOT_PATH / "ideas.json")
 
 # Tests for show() (show idea)
 def test_idea_show(runner: CliRunner) -> None:
     """Showing all ideas should return 0, if it is valid in nature."""
     result = runner.invoke(idea, ["show"])
     assert result.exit_code == 0
+
+def test_idea_show_FileNotFoundError(runner: CliRunner) -> None:
+    """Showing all ideas but ideas.json doesnt exist should return 0, if it is valid in nature."""
+    os.rename(ROOT_PATH / "ideas.json", ROOT_PATH / "_ideas.json")
+    result = runner.invoke(idea, ["show"])
+    assert result.exit_code == 0
+    os.remove(ROOT_PATH / "ideas.json")
+    os.rename(ROOT_PATH / "_ideas.json", ROOT_PATH / "ideas.json")
